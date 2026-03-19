d13424671571be424022d4ba87ddc390c2391462
Sneha Goregaonkar <sneha.goregaonkar@oxygean.com>
2026-03-19 16:28:18 +0530

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://yourdomain.com",
+  siteUrl: "https://oxygean.com",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
